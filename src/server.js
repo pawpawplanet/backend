62a98a1ae25e3fb29340250a1832928d0eb4984c
JC <144067507+lifetimingwhisper@users.noreply.github.com>
2025-03-26 11:43:49 +0000

    調整測試 url

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const http = require("http");
 const server = http.createServer(function(request,response){
     response.setHeader('Content-Type', 'application/json');
     response.setHeader('charset', 'utf-8');
-
-    if (request.method === 'GET' && request.url === '/') {
+    console.log('request.url: ', request.url)
+    if (request.method === 'GET' && request.url === '/api') {
         response.writeHead(200);
         response.end(JSON.stringify({ 
             message: "Hello World from Node.js!",
