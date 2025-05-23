1cc11c0b6f75457d2e1064c267df73ec8ed2bbf1
JC <lifetimingwhispering@gmail.com>
2025-05-23 15:10:07 +0100

    feature/api/get schedule (#20)
    * refine codes
    
    * 新增 API：取得指定保姆的可接案日期

--- src/routes/freelancers.js
+++ src/routes/freelancers.js
@@ -6,9 +6,12 @@ const authenticateToken = require('../middlewares/auth')
 router.get('/profile', authenticateToken, freelancers.getFreelancerProfile)
 router.post('/profile', authenticateToken, freelancers.postFreelancerProfile)
 router.patch('/profile', authenticateToken, freelancers.updateFreelancerProfile)
+
 router.get('/orders', authenticateToken, freelancers.getOrders)
 
 router.post('/services', authenticateToken,freelancers.createOrUpdateService)
 router.get('/services/:id', authenticateToken,freelancers.getFreelancerServiceDetail)
+
+router.get('/:id/schedule', authenticateToken, freelancers.getSchedule)
 
 module.exports = router
