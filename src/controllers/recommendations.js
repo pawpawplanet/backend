502617fb802179c7eb95fb88bd198c82241c82c0
JarvisYu1993 <t5138169@gmail.com>
2025-06-02 12:15:26 +0800

    adjust: judgment and evaluation criteria

--- src/controllers/recommendations.js
+++ src/controllers/recommendations.js
@@ -46,7 +46,7 @@ async function getRecommendations(req, res, next) {
             }
         })
 
-        // services = services.filter((item) => item.rating >= 4.5)
+        services = services.filter((item) => item.rating >= 4.8)
 
         let result = [];
         if (services.length > 10) {
